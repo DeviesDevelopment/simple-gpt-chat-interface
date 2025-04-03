52625e3a9a0fb08dbc78e17684037632d6295e80
dvctokbok <drikvy.cappenberg@tocaboca.com>
2025-04-03 10:50:11 +0200

    adjust global css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'chat',
+    assetPrefix: '/',
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
